5c3f1944c62da73fec6e36778723e0db05b0ee3a
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-08-22 20:43:11 -0300

    feat: criando inputs

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,16 @@ export default function Home() {
       </div>
       <div className="w-7/12 h-full p-6">
         <Tabs tabs={tabsRegister} />
+        <div className="w-full justify-center flex flex-col mt-5">
+          <label htmlFor="nameEnterprise" className="text-xs font-extralight">
+            Nome do seu estabelecimento
+          </label>
+          <input
+            name="nameEnterprise"
+            type="text"
+            className="border rounded-md border-gray-system w-9/12"
+          />
+        </div>
       </div>
     </main>
   )
